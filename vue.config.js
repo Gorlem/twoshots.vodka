8039f0ecbee03a5775ad588a48112067e925dc7b
Dennis Dörr <doerr@kuennecke.com>
2020-11-28 10:56:50 +0100

    fix style

--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,22 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    chainWebpack: config => {
-        config
-            .entry("app")
-            .clear()
-            .add("./src/client/main.js")
-            .end();
-        config
-            .resolve.alias
-                .set("@", path.join(__dirname, "./src/client"));
+  chainWebpack: (config) => {
+    config
+      .entry('app')
+      .clear()
+      .add('./src/client/main.js')
+      .end();
+    config
+      .resolve.alias
+      .set('@', path.join(__dirname, './src/client'));
+  },
+  devServer: {
+    proxy: {
+      '^/socket.io': {
+        target: 'http://localhost:3000',
+        ws: true,
+      },
     },
-    devServer: {
-        proxy: {
-            '^/socket.io': {
-                target: 'http://localhost:3000',
-                ws: true,
-            }
-        },
-    },
-}
+  },
+};
